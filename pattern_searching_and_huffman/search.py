3f6ee77e0795d6b86da001bbf7db31dd28e6fc72
szumili <113642006+szumili@users.noreply.github.com>
2025-06-04 15:50:33 +0200

    Update search.py

--- pattern_searching_and_huffman/search.py
+++ pattern_searching_and_huffman/search.py
@@ -4,16 +4,12 @@ from read_data import read_data
 from pattern_searching import rabin_karp, kmp
 from huffman import huffman
 
-if __name__ == "__main__":
+def search(file_name, pattern, search_type, huffman_coding, save_as):
 
-    file_name = 'data.csv'
-    search_type = 'rk' # rk - rabin karp, kmp - knuth morris pratt
-    huffman_coding = True
-    save_as = search_type + '_search.csv'
+    print('Searching...')
 
     text = read_data(file_name) # read data from file
-    pattern = 'field'
-
+    
     if huffman_coding:
         data = text.copy()
         data.append(pattern)
@@ -33,6 +29,7 @@ if __name__ == "__main__":
     if os.path.exists(save_as):
         os.remove(save_as)
 
+    num_of_lines_containing_pattern = 0
 
     for i in range(len(text)):
         line = text_ready[i]
@@ -43,8 +40,9 @@ if __name__ == "__main__":
 
         if result:
             with open(save_as, 'a') as file:
+                num_of_lines_containing_pattern += 1
                 file.write(text[i] + "\n")
 
     print('The lines in which the search word was found were saved to the file:', save_as)
-
+    return num_of_lines_containing_pattern
 
